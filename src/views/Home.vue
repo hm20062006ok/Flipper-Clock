56e2c2801e52e8fb0025e541c4bc6affa58e25e2
HuMiao <humiao2006@gmail.com>
2022-03-03 14:53:47 +0800

    写完注释

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,6 @@
       <div class='digital front' :class='_textClass(frontTextFromData)'></div>
       <div class='digital back' :class='_textClass(backTextFromData)'></div>
     </div>
-    <button @click='isFlipping = !isFlipping'>翻转</button>
   </div>
 </template>
 
@@ -22,13 +21,35 @@ export default {
       flipType: 'down',
       isFlipping: false,
       frontTextFromData: 0,
-      backTextFromData: 1
+      backTextFromData: 1,
+      duration: 600
     }
   },
   methods:{
     _textClass(number){
-      return 'number'+number
-    }
+      return 'number' + number
+    },
+    _flip(type, front, back) {
+      // 如果处于翻转中，则不执行
+      if (this.isFlipping) {
+        return false
+      }
+      this.frontTextFromData = front
+      this.backTextFromData = back
+      // 根据传递过来的type设置翻转方向
+      this.flipType = type
+      // 设置翻转状态为true
+      this.isFlipping = true
+      setTimeout(() => {
+        // 设置翻转状态为false
+        this.isFlipping = false
+        this.frontTextFromData = back
+      }, this.duration)
+    },
+    // 下翻牌
+    flipDown(front, back) {
+      this._flip('down', front, back)
+    },
   }
 }
 </script>
@@ -50,7 +71,7 @@ export default {
 
 /* 数字 */
 .M-Flipper .digital:before,
-.M-Flipper .digital:before {
+.M-Flipper .digital:after {
   content: "";
   position: absolute;
   left: 0;
@@ -75,21 +96,28 @@ export default {
   line-height: 0;
 }
 
+/* 前牌 上部分 在第一层*/
 .M-Flipper.down .front:before {
   z-index: 3;
 }
-.M-Flipper.down .front:after,
-.M-Flipper.down .back:before{
-  z-index: 1;
-}
+
+/* 后排 上部分 在第一层*/
 .M-Flipper.down .back:after {
   z-index: 2;
   transform-origin: 50% 0%;
   transform: perspective(160px) rotateX(180deg);
 }
 
-/* down下翻  go进行中， 前牌的样式*/
-.M-Flipper.down.go .front:before{
+/* 前牌 下部分 在第二层 */
+/* 后排* 上部分 在第二层 */
+.M-Flipper.down .front:after,
+.M-Flipper.down .back:before{
+  z-index: 1;
+}
+
+
+/* 添加go类时 前牌上部分 绕x轴 旋转180度 最终停住到动画最后一帧的效果*/
+.M-Flipper.down.go .front:before {
   /* 旋转原点定位到x轴的50%， y轴的100%。 也就是底边中点*/
   transform-origin: 50% 100%;
   box-shadow: 0 -2px 6px rgba(255,255,255, 0.3);
@@ -98,6 +126,12 @@ export default {
   /*  元素的背面是其正面的镜像，backface-visibility指示背面的可见性*/
   backface-visibility: hidden;
   -webkit-backface-visibility: hidden;
+}
+
+.M-Flipper.down.go .back:after {
+  animation: backFlipDown 0.6s ease-in-out both;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
 }
 
 @keyframes frontFlipDown {
@@ -111,8 +145,53 @@ export default {
   }
 }
 
+@keyframes backFlipDown {
+  0% {
+    transform: perspective(160px) rotateX(180deg);
+  }
+  100% {
+    transform: perspective(160px) rotateX(0deg);
+  }
+}
+
 .M-Flipper .number0:before,
 .M-Flipper .number0:after {
   content: '0';
+}
+.M-Flipper .number1:before,
+.M-Flipper .number1:after {
+  content: '1';
+}
+.M-Flipper .number2:before,
+.M-Flipper .number2:after {
+  content: '2';
+}
+.M-Flipper .number3:before,
+.M-Flipper .number3:after {
+  content: '3';
+}
+.M-Flipper .number4:before,
+.M-Flipper .number4:after {
+  content: '4';
+}
+.M-Flipper .number5:before,
+.M-Flipper .number5:after {
+  content: '5';
+}
+.M-Flipper .number6:before,
+.M-Flipper .number6:after {
+  content: '6';
+}
+.M-Flipper .number7:before,
+.M-Flipper .number7:after {
+  content: '7';
+}
+.M-Flipper .number8:before,
+.M-Flipper .number8:after {
+  content: '8';
+}
+.M-Flipper .number9:before,
+.M-Flipper .number9:after {
+  content: '9';
 }
 </style>
